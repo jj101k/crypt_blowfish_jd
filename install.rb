a0e440bee2526d945dcdb47ab060ad6fa2584fdf
Jim Driscoll <jim.a.driscoll@gmail.com>
2011-04-10 17:35:04 +0100

    Always ask for 0644

--- install.rb
+++ install.rb
@@ -4,10 +4,10 @@ require "fileutils"
 include FileUtils::Verbose
 
 mkdir_p(CONFIG["sitelibdir"]+"/crypt")
-install("blowfish.rb", CONFIG["sitelibdir"]+"/crypt")
+install("blowfish.rb", CONFIG["sitelibdir"]+"/crypt", :mode => 0644)
 if(File.exists? "Makefile")
 	system((ENV["MAKE"]||"make")+' install')
 else
 	mkdir_p(CONFIG["sitelibdir"]+"/crypt/blowfish")
-	install("core.rb", CONFIG["sitelibdir"]+"/crypt/blowfish/")
+	install("core.rb", CONFIG["sitelibdir"]+"/crypt/blowfish/", :mode => 0644)
 end
