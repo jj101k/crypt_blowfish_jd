7c9100d631b4a1962cd3ded6e1f5d856e6313567
Jim Driscoll <jim.a.driscoll@gmail.com>
2023-09-02 09:47:51 +0100

    Declare block sizes

--- blowfish_prekey.rb
+++ blowfish_prekey.rb
@@ -5,6 +5,11 @@ require "jdcrypt/blowfish/core" unless defined? JdCrypt::Blowfish::Core
 class JdCrypt
   # Provides Blowfish encryption support
   class Blowfish
+    # Provides a list of block sizes (bytes) which are supported
+    def self.block_sizes_supported
+      [64 / 8]
+    end
+
     def initialize(key)
       p "Debugging" if $DEBUG
       throw :too_few_digits if PiDigits.length < Core.needed_pi_digits
@@ -14,6 +19,10 @@ class JdCrypt
         p @derivedkey.sboxes
         p @derivedkey.subkeys
       end
+    end
+
+    def block_sizes_supported
+      Blowfish.block_sizes_supported
     end
 
     def encrypt(string)
